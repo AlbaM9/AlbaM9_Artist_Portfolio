860880b78a9beaa2cace325ab0c9627c88547357
Alba Melchor Gomez <albamelchorgomez@gmail.com>
2024-09-23 15:38:16 +0200

    to finish design main page

--- web/src/components/Banners.js
+++ web/src/components/Banners.js
@@ -6,7 +6,7 @@ function Banners({ backgroundImage, title }) {
     return (
         <section className="flex justify-center items-center">
             <div
-                className="w-[45vw] h-[80vh] bg-cover bg-top bg-no-repeat rounded-full flex flex-col justify-center items-center"
+                className="w-[43vw] h-[80vh] bg-cover bg-top bg-no-repeat rounded-full flex flex-col justify-center items-center"
                 style={{ backgroundImage: `url(${backgroundImage})` }}
             >
                 <h1 className="text-2xl md:text-3xl text-white border-2 border-white px-4 py-2 rounded-lg cursor-pointer transition-transform duration-300 ease-in-out hover:bg-white hover:text-black hover:border-black hover:opacity-80 transform hover:scale-110">
--- web/src/components/Caroussel.js
+++ web/src/components/Caroussel.js
@@ -5,9 +5,45 @@ import dynamic from "next/dynamic";
 const Slider = dynamic(() => import("react-slick"), { ssr: false });
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import '../styles/caroussel.scss';
+//import '../styles/caroussel.scss';
 
 import Banners from "./Banners";
+
+const PreviousArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={`${className}  flex items-center justify-center w-12 h-12 rounded-full z-10 cursor-pointer text-3xl`} // Clases de Tailwind
+            style={{
+                ...style,
+                display: "block",
+                left: "450px",
+                transform: "scale(2)", // Escala a 1.5 veces el tamaño original
+                // Ajusta la posición a la izquierda (ajusta según sea necesario)
+            }}
+            onClick={onClick}
+        >
+
+        </div>
+    );
+};
+
+const NextArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={`${className} p-2 z-10 cursor-pointer w-3`} // Clases de Tailwind
+            style={{
+                ...style,
+                display: "block",
+                right: "450px", transform: "scale(2) ", // Ajusta la posición a la derecha (ajusta según sea necesario)
+            }}
+            onClick={onClick}
+        >
+            &#10095; {/* Símbolo de flecha hacia la derecha */}
+        </div>
+    );
+};
 
 const Caroussel = () => {
     const [mounted, setMounted] = useState(false);
@@ -17,24 +53,25 @@ const Caroussel = () => {
     }, []);
 
     const settings = {
-        dots: true,
+        dots: false, // Desactiva los dots
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
+        arrows: true, // Activa las flechas
+        prevArrow: <PreviousArrow />, // Flecha personalizada para retroceder
+        nextArrow: <NextArrow />,     // Flecha personalizada para avanzar
     };
 
     if (!mounted) return null; // Evita renderizar en el servidor
 
+
     return (
         <>
-
-            <h1>Jobs</h1>
+            <h1 class="text-white text-3xl text-center mb-[5vh]">Jobs</h1>
             <div className="carousel-container">
-
-
                 <Slider {...settings}>
                     <div>
                         <Banners
@@ -50,12 +87,9 @@ const Caroussel = () => {
                             link="">
                         </Banners>
                     </div>
-
                 </Slider>
             </div>
-
         </>
-
     );
 };
 
